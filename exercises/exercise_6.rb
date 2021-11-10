d67111bc9837874a90776b0cb448d4961609dc76
Roya Chobineh <roya.chbn@gmail.com>
2021-11-09 19:34:16 -0500

    Done

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.employees.create(first_name: "Roya", last_name: "Ch", hourly_rate: 60)
+@store1.employees.create(first_name: "Farah", last_name: "Gh", hourly_rate: 10)
+@store1.employees.create(first_name: "Amir", last_name: "Ph", hourly_rate: 40)
+
+@store1.employees.each {|x| puts "#{x.first_name}"}
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,6 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@new_store = Store.create(name: "Hobos", annual_revenue: 10000, mens_apparel:true)
+
+@new_store.save!
